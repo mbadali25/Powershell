a12f2752503e927999f5f2a5f1be903f6407e760
Matthew Badali <mbadali25@gmail.com>
2021-05-19 12:25:53 -0500

    Create Install-Chocolatey.ps1

---
+++ Systems Management/Chocolatey/Install-Chocolatey.ps1
@@ -0,0 +1,9 @@
+##############################################
+##  Install Chocolatey Package Manager
+##
+## This can be found here 
+## 
+## https://chocolatey.org/install
+##
+############################################
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
